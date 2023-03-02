9e20d14241f96b40f3a65f42c27736ceb9a3ed5c
boranuzun <boran.u3@gmail.com>
2023-03-02 13:14:22 +0100

    Update

--- roomSchedule.sh
+++ roomSchedule.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? "; read -r choix
+echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? : "; read -r choix
 echo -n "Choisir la salle: "; read -r salleNom
+printf "\n"
 
 if [ "$choix" = "t" ]; then
-    curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup 'p text{}'
     room=$(curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup ':contains("Salle") text{}')
 
     if [ "$room" = "Salle occupée" ]; then
@@ -12,6 +12,7 @@ if [ "$choix" = "t" ]; then
     else
         tput setaf 2; tput bold; curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup ':contains("Salle") text{}'
     fi
+    tput setaf 7; curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup 'p text{}'
 else
     open https://www.hesge.ch/heg/salle/"$salleNom"
 fi
--- roomScheduleArgv.sh
+++ roomScheduleArgv.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
-echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? "; read -r choix
+echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? : "; read -r choix
+printf "\n"
 
 if [ "$choix" = "t" ]; then
-    curl -s https://www.hesge.ch/heg/salle/"$1" | pup 'p text{}'
     room=$(curl -s https://www.hesge.ch/heg/salle/"$1" | pup ':contains("Salle") text{}')
-
     if [ "$room" = "Salle occupée" ]; then
         tput setaf 1; tput bold; curl -s https://www.hesge.ch/heg/salle/"$1" | pup ':contains("Salle") text{}'
     else
         tput setaf 2; tput bold; curl -s https://www.hesge.ch/heg/salle/"$1" | pup ':contains("Salle") text{}'
     fi
+    tput setaf 7; curl -s https://www.hesge.ch/heg/salle/"$1" | pup 'p text{}'
 else
     open https://www.hesge.ch/heg/salle/"$1"
 fi
