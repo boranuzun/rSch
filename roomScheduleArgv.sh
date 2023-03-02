d94b52e239af72aa8a844167e15950e69182631f
boranuzun <boran.u3@gmail.com>
2023-03-02 14:50:41 +0100

    Added second positional parameter

--- roomScheduleArgv.sh
+++ roomScheduleArgv.sh
@@ -1,16 +1,17 @@
 #! /usr/bin/env bash
 
-echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? : "; read -r choix
-printf "\n"
+choix=$1
+# $1 is the first argument passed to the script (t or n)
+# $2 is the first argument passed to the script (the room name)
 
-if [ "$choix" = "t" ]; then
-    room=$(curl -s https://www.hesge.ch/heg/salle/"$1" | pup ':contains("Salle") text{}')
+if [ "$choix" = "-t" ]; then
+    room=$(curl -s https://www.hesge.ch/heg/salle/"$2" | pup ':contains("Salle") text{}')
     if [ "$room" = "Salle occupée" ]; then
-        tput setaf 1; tput bold; curl -s https://www.hesge.ch/heg/salle/"$1" | pup ':contains("Salle") text{}'
+        tput setaf 1; tput bold; curl -s https://www.hesge.ch/heg/salle/"$2" | pup ':contains("Salle") text{}'
     else
-        tput setaf 2; tput bold; curl -s https://www.hesge.ch/heg/salle/"$1" | pup ':contains("Salle") text{}'
+        tput setaf 2; tput bold; curl -s https://www.hesge.ch/heg/salle/"$2" | pup ':contains("Salle") text{}'
     fi
-    tput setaf 7; curl -s https://www.hesge.ch/heg/salle/"$1" | pup 'p text{}'
+    tput setaf 7; curl -s https://www.hesge.ch/heg/salle/"$2" | pup 'p text{}'
 else
-    open https://www.hesge.ch/heg/salle/"$1"
+    open https://www.hesge.ch/heg/salle/"$2"
 fi
