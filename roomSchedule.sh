9820b07b693113216122708253f320ab57068c31
boranuzun <boran.u3@gmail.com>
2023-03-02 11:26:15 +0100

    Added color

--- roomSchedule.sh
+++ roomSchedule.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env bash
 
-echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? "; read choix
-echo -n "Choisir la salle: "; read salleNom
+echo -n "Afficher dans le terminal (t) ou dans le navigateur (n) ? "; read -r choix
+echo -n "Choisir la salle: "; read -r salleNom
 
-if [ $choix = "t" ]; then
-    curl -s https://www.hesge.ch/heg/salle/$salleNom | pup -c ':contains("Horaire"), p, :contains("Salle") text{}'
+if [ "$choix" = "t" ]; then
+    curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup ':contains("Horaire"), p text{}'
+    room=$(curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup ':contains("Salle") text{}')
+
+    if [ "$room" = "Salle occupée" ]; then
+        tput setaf 1; curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup ':contains("Salle") text{}'
+    else
+        tput setaf 2; curl -s https://www.hesge.ch/heg/salle/"$salleNom" | pup ':contains("Salle") text{}'
+    fi
 else
-    open https://www.hesge.ch/heg/salle/$salleNom
+    open https://www.hesge.ch/heg/salle/"$salleNom"
 fi
